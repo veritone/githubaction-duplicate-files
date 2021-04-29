770c89946c4b75661c9786b6aba756ec88e152d7
Al Brown <abrown@veritone.com>
2021-04-28 23:37:41 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,13 +9,10 @@ echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/review
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
-echo '::group:: Installing golangci-lint ... https://github.com/golangci/golangci-lint'
-curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${GOLANGCI_LINT_VERSION}" 2>&1
-echo '::endgroup::'
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo '::group:: Running golangci-lint with reviewdog 🐶 ...'
+echo '::group:: Running dupe-files with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
 golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
   | reviewdog -f=golangci-lint \
