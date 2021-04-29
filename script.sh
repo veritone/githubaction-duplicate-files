bd473451fad886fbae313cd617a59b7139bd8ce5
Al Brown <abrown@veritone.com>
2021-04-28 23:47:57 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,8 +14,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo '::group:: Running dupe-files with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
-golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
-  | reviewdog -f=golangci-lint \
+bash check.sh ${INPUT_DIRECTORY:sql} | reviewdog -f=golangci-lint \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE:-added}" \
